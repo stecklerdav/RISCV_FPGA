10ff6b906950460fc24edfb788a33a9c9bd87fc0
David Steckler <you@example.com>
2026-03-29 23:26:07 +0100

    update

--- RVI20U32/rv32i_vscode/prog_asm.asm
+++ RVI20U32/rv32i_vscode/prog_asm.asm
@@ -5,9 +5,14 @@ prog_asm.elf:     file format elf32-littleriscv
 Disassembly of section .text:
 
 00000000 <__text_start>:
-   0:	00100093          	addi	x1,x0,1
-   4:	00200113          	addi	x2,x0,2
+   0:	00500093          	addi	x1,x0,5
+   4:	00500113          	addi	x2,x0,5
+   8:	00208663          	beq	x1,x2,14 <target>
+   c:	00300193          	addi	x3,x0,3
+  10:	00400213          	addi	x4,x0,4
 
-00000008 <target>:
-   8:	00300193          	addi	x3,x0,3
-   c:	ffdff06f          	jal	x0,8 <target>
+00000014 <target>:
+  14:	00500293          	addi	x5,x0,5
+
+00000018 <loop>:
+  18:	0000006f          	jal	x0,18 <loop>
